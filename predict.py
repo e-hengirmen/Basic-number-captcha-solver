5d52caa6f6b484ebf67d6f624005b1e35e2c2c35
e-hengirmen <ehengirmen@hotmail.com>
2025-01-01 20:14:21 +0300

    now suppressing tensorflow warnings

--- predict.py
+++ predict.py
@@ -1,5 +1,24 @@
-import tensorflow as tf
-from tensorflow.keras.preprocessing.image import img_to_array, load_img
+import os
+import sys
+class SuppressOutput:
+    def __enter__(self):
+        self._original_stdout = sys.stdout
+        self._original_stderr = sys.stderr
+        sys.stdout = open(os.devnull, 'w')
+        sys.stderr = open(os.devnull, 'w')
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        sys.stdout.close()
+        sys.stderr.close()
+        sys.stdout = self._original_stdout
+        sys.stderr = self._original_stderr
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+with SuppressOutput():
+    import tensorflow as tf
+    from tensorflow.keras.preprocessing.image import img_to_array, load_img
+
 import Preprocess
 
 import matplotlib.pyplot as plt
